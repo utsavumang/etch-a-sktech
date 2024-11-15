3fba8206c3d4b7342e514d5ba22d72b71e7fd682
utsavumang <utsavumangpandey@gmail.com>
2024-11-15 22:53:36 +0530

    set up canvas and coloring logic

---
+++ javascript.js
@@ -0,0 +1,28 @@
+container =  document.querySelector('.container')
+
+
+for (let i = 0; i < 16; i++){
+    const divrow = document.createElement("div");
+    container.appendChild(divrow);
+    divrow.classList.add("row");
+
+    for (let i = 0; i < 16; i++) {
+        const divcell = document.createElement("div")
+        divrow.appendChild(divcell);
+        divcell.classList.add("cell")
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        divcell.style.backgroundColor = '#'+randomColor;
+    }
+}
+
+container.addEventListener("click", function(e) {
+
+    let targ = e.target;
+
+    darken(targ);
+})
+
+function darken(box) {
+
+    box.style.backgroundColor = 'red';
+}
